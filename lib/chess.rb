9b4e3e9c91870d54b0211efa6b4d41d331e8fc4f
brian <lbrian357@gmail.com>
2016-08-31 20:14:48 +0300

    bishop feature added, next to add queen feature

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,18 +1,18 @@
 class ChessSymbols
   attr_accessor :w_king, :w_queen, :w_rook, :w_bishop, :w_pawn, :b_king, :b_queen, :b_rook, :b_bishop, :b_knight, :b_pawn
   def initialize
-  @w_king = "♔"
-  @w_queen = "♕"
-  @w_rook = "♖"
-  @w_bishop = "♗"
-  @w_knight = "♘"
-  @w_pawn = "♙"
-  @b_king = "♚"
-  @b_queen = "♛"
-  @b_rook = "♜"
-  @b_bishop = "♝"
-  @b_knight = "♞"
-  @b_pawn = "♟"
+    @w_king = "♔"
+    @w_queen = "♕"
+    @w_rook = "♖"
+    @w_bishop = "♗"
+    @w_knight = "♘"
+    @w_pawn = "♙"
+    @b_king = "♚"
+    @b_queen = "♛"
+    @b_rook = "♜"
+    @b_bishop = "♝"
+    @b_knight = "♞"
+    @b_pawn = "♟"
   end
 
 end
@@ -40,22 +40,24 @@ class Game < ChessSymbols
     @mid_row = "├───┼───┼───┼───┼───┼───┼───┼───┤"
     @top_row = "┌───┬───┬───┬───┬───┬───┬───┬───┐"
     @bottom_row = "└───┴───┴───┴───┴───┴───┴───┴───┘"
-@symbols = {
-  'w_king' => "♔",
-  'w_queen '=> "♕",
-  'w_rook' => "♖",
-  'w_bishop' => "♗",
-  'w_knight' => "♘",
-  'w_pawn' => "♙",
-  'b_king' => "♚",
-  'b_queen' => "♛",
-  'b_rook' => "♜",
-  'b_bishop' => "♝",
-  'b_knight' => "♞",
-  'b_pawn' => "♟"
-}
-@p1 = Player.new('white')
-@p2 = Player.new('black')
+    @w_symbols = {
+      'w_king' => "♔",
+      'w_queen '=> "♕",
+      'w_rook' => "♖",
+      'w_bishop' => "♗",
+      'w_knight' => "♘",
+      'w_pawn' => "♙",
+    }
+    @b_symbols = {
+      'b_king' => "♚",
+      'b_queen' => "♛",
+      'b_rook' => "♜",
+      'b_bishop' => "♝",
+      'b_knight' => "♞",
+      'b_pawn' => "♟"
+    }
+    @p1 = Player.new('white')
+    @p2 = Player.new('black')
   end
 
   def draw_board
@@ -65,45 +67,45 @@ class Game < ChessSymbols
   def find_col(col)
     if col == 'a'
       return 2
-      elsif col == 'b'
+    elsif col == 'b'
       return 6
-      elsif col == 'c'
+    elsif col == 'c'
       return 10
-      elsif col == 'd'
+    elsif col == 'd'
       return 14
-      elsif col == 'e'
+    elsif col == 'e'
       return 18
-      elsif col == 'f'
+    elsif col == 'f'
       return 22
-      elsif col == 'g'
+    elsif col == 'g'
       return 26
-      elsif col == 'h'
+    elsif col == 'h'
       return 30
     end
   end
-  
+
   def find_row(number)
     if number == 1
       return row1
     elsif number == 2
       return row2
-      elsif number == 3
+    elsif number == 3
       return row3 
-      elsif number == 4
+    elsif number == 4
       return row4
-      elsif number == 5
+    elsif number == 5
       return row5
-      elsif number == 6
+    elsif number == 6
       return row6
-      elsif number == 7
+    elsif number == 7
       return row7
-      elsif number == 8
+    elsif number == 8
       return row8
     end
   end
 
   def find_piece(col, row)
-    find_row(row)[find_col(col)]
+    find_row(row.to_i)[find_col(col)]
   end
 
   def place(piece, col, row)
@@ -119,25 +121,248 @@ class Game < ChessSymbols
     remove(col, row)
   end
 
+  def rook_moves(turn)
+    moves_ary = []
+    col_ary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    row_ary = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    i = col_ary.index(turn[0])
+    i = i + 1
+    condition = true
+    while i < 8 && condition == true #for cases to the rooks right
+      if find_piece(col_ary[i], turn[1]) == ' '
+        moves_ary << "#{col_ary[i]}#{turn[1]}"
+      elsif @b_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+        moves_ary << "#{col_ary[i]}#{turn[1]}"
+      elsif @b_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+
+      elsif @w_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♖'
+
+        condition = false
+      elsif @w_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+        moves_ary << "#{col_ary[i]}#{turn[1]}"
+      end
+      i += 1
+    end 
+
+    i = col_ary.index(turn[0])
+    i = i - 1
+    condition = true
+    while i >= 0 && condition == true#for cases to the rooks left
+      if find_piece(col_ary[i], turn[1]) == ' '
+        moves_ary << "#{col_ary[i]}#{turn[1]}"
+      elsif @b_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+        moves_ary << "#{col_ary[i]}#{turn[1]}"
+      elsif @w_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+      elsif @b_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+      elsif @w_symbols.values.include?(find_piece(col_ary[i], turn[1])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+        moves_ary << "#{col_ary[i]}#{turn[1]}"
+      end
+      i -= 1
+    end
+
+    j = row_ary.index(turn[1].to_i) #remember to turn all turn[1].to_i
+    j = j + 1
+    condition = true
+    while j < 8 && condition == true #for all possible cases of the rook moving up
+      if find_piece(turn[0], row_ary[j]) == ' '
+        moves_ary << "#{turn[0]}#{row_ary[j]}"
+      elsif @b_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+        moves_ary << "#{turn[0]}#{row_ary[j]}"
+      elsif @w_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+      elsif @b_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+      elsif @w_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+        moves_ary << "#{turn[0]}#{row_ary[j]}"
+      end
+      j += 1
+    end
+
+    j = row_ary.index(turn[1].to_i)
+    j = j - 1
+    condition = true
+    while j >= 0 && condition == true #for all possible movements of the rock going down
+      if find_piece(turn[0], row_ary[j]) == ' '
+        moves_ary << "#{turn[0]}#{row_ary[j]}"
+      elsif @b_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+        moves_ary << "#{turn[0]}#{row_ary[j]}"
+      elsif @w_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♖'
+        condition = false
+      elsif @b_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+      elsif @w_symbols.values.include?(find_piece(turn[0], row_ary[j])) && find_piece(turn[0], turn[1]) == '♜'
+        condition = false
+        moves_ary << "#{turn[0]}#{row_ary[j]}"
+      end
+      j -= 1
+
+    end
+    moves_ary
+  end
+
+  def bishop_moves(turn)
+    moves_ary = []
+    col_ary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    row_ary = [1, 2, 3, 4, 5, 6, 7, 8]
+    #possible moves towards top left
+    condition = true
+    c = col_ary.index(turn[0])
+    r = row_ary.index(turn[1].to_i)
+    c = c - 1
+    r = r + 1
+    while condition == true && c >= 0 && r < 8
+      if find_piece(col_ary[c], row_ary[r]) == ' '
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♗' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      elsif find_piece(turn[0], turn[1]) == '♗' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      end
+      c -= 1
+      r += 1
+    end
+    #possible moves towards top right
+    condition = true
+    c = col_ary.index(turn[0])
+    r = row_ary.index(turn[1].to_i)
+    c = c + 1
+    r = r + 1
+    while condition == true && c < 8 && r < 8
+      if find_piece(col_ary[c], row_ary[r]) == ' '
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♗' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      elsif find_piece(turn[0], turn[1]) == '♗' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      end
+      c += 1
+      r += 1
+    end
+    #possible moves towards bottom left
+    condition = true
+    c = col_ary.index(turn[0])
+    r = row_ary.index(turn[1].to_i)
+    c = c - 1
+    r = r - 1
+    while condition == true && c >= 0 && r >= 0
+      if find_piece(col_ary[c], row_ary[r]) == ' '
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♗' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      elsif find_piece(turn[0], turn[1]) == '♗' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      end
+      c -= 1
+      r -= 1
+    end
+    #possible moves towards bottom right
+    condition = true
+    c = col_ary.index(turn[0])
+    r = row_ary.index(turn[1].to_i)
+    c = c + 1
+    r = r - 1
+    while condition == true && c < 8 && r >= 0
+      if find_piece(col_ary[c], row_ary[r]) == ' '
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♗' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      elsif find_piece(turn[0], turn[1]) == '♗' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @w_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+        moves_ary << "#{col_ary[c]}#{row_ary[r]}"
+      elsif find_piece(turn[0], turn[1]) == '♝' && @b_symbols.values.include?(find_piece(col_ary[c], row_ary[r]))
+        condition = false
+      end
+      c += 1
+      r -= 1
+    end
+    moves_ary
+  end
+    
+
+  def possible_moves(turn)
+        case find_piece(turn[0], turn[1])
+
+          #rook_moves
+        when '♖' || '♜' then return rook_moves(turn)
+        when '♗'
+          #bishop_moves
+
+        when '♕'
+          #queen_moves
+        when '♔'
+          #king_moves
+        when '♘'
+          #knight_moves
+        when '♙'
+          #pawn_moves
+          #if on row 2 it can move two spaces
+          #if a piece is diagonal to it, it can take it
+          #if it gets to the end it can become a queen
+        end
+
+  end
+
+
   def start
     count = 1
     while p1.victory == false && p2.victory == false
       draw_board
       if count.odd?
         print 'white turn, what is your move? '
-        turn = gets.chomp
-        move(turn[0], turn[1].to_i, turn[3], turn[4].to_i)
+        turn = gets.chomp.gsub(/\s+/, "")
+        
+          if possible_moves.include?(turn[-2..-1])
+
+
+
+        move(turn[0], turn[1].to_i, turn[-2], turn[-1].to_i)
+          end
+
       else
         print 'black turn, what is your move? '
-        turn = gets.chomp
-        move(turn[0], turn[1].to_i, turn[3], turn[4].to_i)
+        turn = gets.chomp.gsub(/\s+/, "")
+          if possible_moves.include?(turn[-2..-1])
+        move(turn[0], turn[1].to_i, turn[-2], turn[-1].to_i)
+          end
       end
       count += 1
     end
   end
 end
-a = Game.new
-a.start
+#a = Game.new
+#a.start
 
 =begin
 │ ─ ┌ ┐ └ ┘ ┬ ┴ ├ ┤ ┼
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -10,7 +10,7 @@ describe 'Game' do
     context 'when #draw_board is called' do
       it 'will print out an 8x8 board' do
         expect{@a_game.draw_board}.to output(
-"┌───┬───┬───┬───┬───┬───┬───┬───┐
+          "┌───┬───┬───┬───┬───┬───┬───┬───┐
 │ ♜ │ ♞ │ ♝ │ ♛ │ ♚ │ ♝ │ ♞ │ ♜ │
 ├───┼───┼───┼───┼───┼───┼───┼───┤
 │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │
@@ -27,7 +27,7 @@ describe 'Game' do
 ├───┼───┼───┼───┼───┼───┼───┼───┤
 │ ♖ │ ♘ │ ♗ │ ♕ │ ♔ │ ♗ │ ♘ │ ♖ │
 └───┴───┴───┴───┴───┴───┴───┴───┘"
-).to_stdout
+        ).to_stdout
       end
     end
   end
@@ -35,7 +35,7 @@ describe 'Game' do
   describe '#find_row' do
     context 'when given a number' do
       it 'returns the corresponding row_number' do
-@a_game.row3 = '│   │   │   │   │   │ ♘ │   │   │'
+        @a_game.row3 = '│   │   │   │   │   │ ♘ │   │   │'
         expect(@a_game.find_row(3)).to eql('│   │   │   │   │   │ ♘ │   │   │')
       end
     end
@@ -45,7 +45,7 @@ describe 'Game' do
     context 'when given a letter' do
       it 'returns the corresponding column number in string' do
 
-       expect(@a_game.find_col('c')).to eql(10)
+        expect(@a_game.find_col('c')).to eql(10)
       end
     end
   end
@@ -54,7 +54,7 @@ describe 'Game' do
   describe '#place' do
     context 'when a piece, column and row is given to #place' do
       it 'finds that place on the grid and replaces it with piece' do
-@a_game.place('♘', 'f', 4)
+        @a_game.place('♘', 'f', 4)
         expect(@a_game.row4).to eql('│   │   │   │   │   │ ♘ │   │   │')
       end
     end
@@ -64,7 +64,7 @@ describe 'Game' do
     context 'when a piece at a particular column and row is designated to move' do
       it 'erases that piece from that column and row' do
         @a_game.row5 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
-@a_game.remove('f', 5)
+        @a_game.remove('f', 5)
         expect(@a_game.row5).to eql('│   │   │ ♗ │   │   │   │   │   │')
       end
     end
@@ -73,32 +73,124 @@ describe 'Game' do
   describe '#find_piece' do
     context 'when given a set of coordinates' do
       it 'returns the string that corresponds' do
-      @a_game.row3 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
-      expect(@a_game.find_piece('c', 3)).to eql('♗')
+        @a_game.row3 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
+        expect(@a_game.find_piece('c', 3)).to eql('♗')
       end
     end
   end
-        
+
 
 
   describe '#move' do
     context 'when two sets of coordinates are given' do
       it 'erases the piece from the first set of coordinates' do
-      @a_game.row3 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
+        @a_game.row3 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
         @a_game.move('c', 3, 'd', 4)
         expect(@a_game.row3).to eql('│   │   │   │   │   │ ♘ │   │   │')
       end
 
       it 'writes the piece to the second set of coordinates' do
-      @a_game.row3 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
+        @a_game.row3 = '│   │   │ ♗ │   │   │ ♘ │   │   │'
         @a_game.move('c', 3, 'd', 4)
         expect(@a_game.row4).to eql('│   │   │   │ ♗ │   │   │   │   │')
       end
     end
   end
 
+  describe '#rook_moves' do
+    context 'when disignated movement starts with a rook' do
+      it 'will create an array with all possible legal moves to the right' do
+        @a_game.row4 = '│   │   │   │   │   │ ♘ │   │   │'
+        @a_game.row3 = '│   │   │   │   │ ♘ │ ♖ │   │   │'
+        @a_game.row2 = '│   │   │   │   │   │ ♘ │   │   │'
+        expect(@a_game.rook_moves('f3')).to eql(['g3', 'h3'])
+      end
 
+      it 'will create and array of possible moves in all directions that are non diagonal' do
+        @a_game.row5 = '│   │   │   │   │   │ ♘ │   │   │'
+        @a_game.row4 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row3 = '│   │   │   │ ♘ │   │ ♖ │   │   │'
+        @a_game.row2 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row1 = '│   │   │   │   │   │ ♘ │   │   │'
+        expect(@a_game.rook_moves('f3')).to match_array(['g3', 'h3', 'e3', 'f2', 'f4'])
+      end
+
+      it 'will include coordinates where there are black pieces to be captured' do 
+        @a_game.row5 = '│   │   │   │   │   │ ♘ │   │   │'
+        @a_game.row4 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row3 = '│   │   │   │ ♘ │   │ ♖ │   │   │'
+        @a_game.row2 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row1 = '│   │   │   │   │   │ ♟ │   │   │'
+        expect(@a_game.rook_moves('f3')).to match_array(['g3', 'h3', 'e3', 'f2', 'f4', 'f1'])
+      end
+
+      it 'works for black pieces as well' do 
+        @a_game.row5 = '│   │   │   │   │   │ ♞ │   │   │'
+        @a_game.row4 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row3 = '│   │   │   │ ♞ │   │ ♜ │   │   │'
+        @a_game.row2 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row1 = '│   │   │   │   │   │ ♙ │   │   │'
+        expect(@a_game.rook_moves('f3')).to match_array(['g3', 'h3', 'e3', 'f2', 'f4', 'f1'])
+      end
+    end
+  end
+
+  describe '#bishop_moves' do 
+    context 'when bishop is the designated piece to be moved' do
+      it 'will show all possible moves to the top left' do
+        @a_game.row8 = '│   │   │ ♘ │   │   │   │   │   │'
+        @a_game.row7 = '│   │ ♗ │   │   │   │   │   │   │'
+        @a_game.row6 = '│ ♘ │   │ ♘ │   │   │   │   │   │'
+        expect(@a_game.bishop_moves('b7')).to match_array(['a8'])
+      end
+
+      it 'will show all possible moves in any diagonal direction' do
+        @a_game.row8 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row7 = '│   │ ♗ │   │   │   │   │   │   │'
+        @a_game.row6 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row5 = '│   │   │   │ ♘ │   │   │   │   │'
+        expect(@a_game.bishop_moves('b7')).to match_array(['a8', 'c8', 'c6', 'a6'])
+      end
+
+      it 'will also include the possibility of taking an opposite colour piece' do
+        @a_game.row8 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row7 = '│   │ ♗ │   │   │   │   │   │   │'
+        @a_game.row6 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row5 = '│   │   │   │ ♞ │   │   │   │   │'
+        expect(@a_game.bishop_moves('b7')).to match_array(['a8', 'c8', 'c6', 'a6', 'd5'])
+      end
+
+      it 'also works for black pieces' do
+        @a_game.row8 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row7 = '│   │ ♝ │   │   │   │   │   │   │'
+        @a_game.row6 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row5 = '│   │   │   │ ♖ │   │   │   │   │'
+        expect(@a_game.bishop_moves('b7')).to match_array(['a8', 'c8', 'c6', 'a6', 'd5'])
+      end
+    end
+
+    describe '#queen_moves' do
+      context 'when selected starting coordinate is a queen' do
+        it 'will show all possible moves (diagonal and horizontal)' do
+        @a_game.row8 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row7 = '│   │ ♕ │   │   │   │   │   │   │'
+        @a_game.row6 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row5 = '│   │   │   │   │   │   │   │   │'
+        @a_game.row4 = '│   │   │   │   │ ♖ │   │   │   │'
+        expect(@a_game.queen_moves('b7')).to match_array(['a8', 'c8', 'c6', 'a6', 'd5'])
+        end
+      end
+    end
+
+  end
 
         
+
+
+
+
+
+
+
 
 end
